f498fab3f1615a87a68bb87efb19262f16cdd571
Francisco <fjramos641@gmail.com>
2024-02-11 18:36:18 -0500

    mi tarea

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import './App.css'
+import LoginForm from './LoginForm/LoginForm'
+
+function App() {
+  return (
+    <>
+      <div>
+           <LoginForm/>
+      </div>
+    </>
+  )
+}
+
+export default App
